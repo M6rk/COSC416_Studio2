dea76d35c7443505adaa9f73089c7f502ec89e7c
Mark <mark.lovesey03@gmail.com>
2025-02-14 17:57:58 -0800

    Launch Indicator

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -3,21 +3,20 @@ using UnityEngine.Events;
 
 public class BallController : MonoBehaviour
 {
+    //private InputManager inputManager;
     [SerializeField] private float force = 1f;
     [SerializeField] private Transform ballAnchor;
+    [SerializeField] private Transform launchIndicator;
+    [SerializeField] private InputManager inputManager;
 
     private bool isBallLaunched;
     private Rigidbody ballRB;
-    private InputManager inputManager;
 
     void Start()
     {
         ballRB = GetComponent<Rigidbody>();
-        inputManager = FindObjectOfType<InputManager>();
-        if (inputManager != null)
-        {
-            inputManager.OnSpacePressed.AddListener(LaunchBall);
-        }
+        //inputManager = FindObjectOfType<InputManager>();
+        inputManager.OnSpacePressed.AddListener(LaunchBall);
         transform.parent = ballAnchor;
         transform.localPosition = Vector3.zero;
         ballRB.isKinematic = true;
@@ -30,5 +29,7 @@ public class BallController : MonoBehaviour
         transform.parent = null;
         ballRB.isKinematic = false;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
+        ballRB.AddForce(launchIndicator.forward * force, ForceMode.Impulse);
+        launchIndicator.gameObject.SetActive(false);
     }
 }
